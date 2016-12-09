6a7fd0d854106159a63dc4c4c6e103578130d106
Harsh Pokharna <harsh.81291@gmail.com>
2016-12-10 00:18:10 +0530

    Server Context

---
+++ java_wallet/src/main/java/wepesa/core/ServerContext.java
@@ -0,0 +1,61 @@
+package wepesa.core;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.logging.LogManager;
+
+public class ServerContext
+{
+    private static Logger LOG = LogManager.getRootLogger();
+
+    public static ServerContext instance;
+
+    public static void init(ExecutorService workerPool)
+    {
+        instance = new ServerContext(workerPool);
+        LOG.debug("[ServerContext initialized]");
+
+        DatabaseAdapter databaseAdapter = DatabaseAdapter.getInstance();
+        try
+        {
+            databaseAdapter.init();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException();
+        }
+    }
+
+    public static void destroy()
+    {
+        getInstance().workerPool.shutdownNow();
+
+        LOG.debug("[ServerContext destroyed]");
+
+        DatabaseAdapter databaseAdapter = DatabaseAdapter.getInstance();
+        databaseAdapter.close();
+    }
+
+    public static ServerContext getInstance()
+    {
+        if (instance == null)
+        {
+            LOG.fatal("[ServerContext not initialized]");
+            throw new IllegalStateException("ServerContext not initialized");
+        }
+        return instance;
+    }
+
+    private ExecutorService workerPool;
+
+    private ServerContext(ExecutorService workerPool)
+    {
+        this.workerPool = workerPool;
+    }
+
+    public ExecutorService getWorkerPool()
+    {
+        return workerPool;
+    }
+}
+
