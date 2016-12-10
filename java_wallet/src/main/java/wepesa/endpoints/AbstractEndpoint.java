36c3ddd7199f5d0c5e003807b69b1708a05a539d
Harsh Pokharna <harsh.81291@gmail.com>
2016-12-11 02:03:13 +0530

    Abstract endpoint

---
+++ java_wallet/src/main/java/wepesa/endpoints/AbstractEndpoint.java
@@ -0,0 +1,44 @@
+package wepesa.endpoints;
+
+import wepesa.core.ServerContext;
+import wepesa.utils.GsonProvider;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutorService;
+
+public class AbstractEndpoint
+{
+    protected ExecutorService workerPool;
+//    protected BlockchainApiManager blockchainApiManager;
+
+    public AbstractEndpoint()
+    {
+        workerPool = ServerContext.getInstance().getWorkerPool();
+//        blockchainApiManager = BlockchainApiManager.getInstance();
+    }
+
+    protected Response buildSuccessJsonResponse(Object o)
+    {
+        return Response.ok(GsonProvider.get().toJson(o), MediaType.APPLICATION_JSON_TYPE).build();
+    }
+
+    protected Response buildEmptySuccessResponse()
+    {
+        return Response.ok().build();
+    }
+
+    protected Response redirect(String url)
+    {
+        try
+        {
+            return Response.seeOther(new URI(url)).build();
+        }
+        catch (URISyntaxException e)
+        {
+            return Response.serverError().build();
+        }
+    }
+}
