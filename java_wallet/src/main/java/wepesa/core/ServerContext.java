948d650ce237d415656d8803fab746c4974b1cd5
Harsh Pokharna <harsh.81291@gmail.com>
2016-12-11 01:54:07 +0530

    Server Context Updated with new dependencies

--- java_wallet/src/main/java/wepesa/core/ServerContext.java
+++ java_wallet/src/main/java/wepesa/core/ServerContext.java
@@ -2,18 +2,19 @@ package wepesa.core;
 
 
 import java.util.concurrent.ExecutorService;
-import java.util.logging.LogManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ServerContext
 {
-//    private static Logger LOG = LogManager.getRootLogger();
+    private static Logger LOG = LogManager.getRootLogger();
 
     public static ServerContext instance;
 
     public static void init(ExecutorService workerPool)
     {
         instance = new ServerContext(workerPool);
-//        LOG.debug("[ServerContext initialized]");
+        LOG.debug("[ServerContext initialized]");
 
 
     }
@@ -22,17 +23,14 @@ public class ServerContext
     {
         getInstance().workerPool.shutdownNow();
 
-//        LOG.debug("[ServerContext destroyed]");
-
-//        DatabaseAdapter databaseAdapter = DatabaseAdapter.getInstance();
-//        databaseAdapter.close();
+        LOG.debug("[ServerContext destroyed]");
     }
 
     public static ServerContext getInstance()
     {
         if (instance == null)
         {
-//            LOG.fatal("[ServerContext not initialized]");
+            LOG.fatal("[ServerContext not initialized]");
             throw new IllegalStateException("ServerContext not initialized");
         }
         return instance;
