ef9344dcd5ab6b9c3dfd94734a8e201ec8dcecac
Harsh Pokharna <harsh.81291@gmail.com>
2016-12-11 20:23:37 +0530

    Health Endpoint

---
+++ java_wallet/src/main/java/wepesa/endpoints/HealthEndPoint.java
@@ -0,0 +1,23 @@
+package wepesa.endpoints;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("health-check")
+public class HealthEndPoint extends AbstractEndpoint {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public void healthCheck(@Suspended AsyncResponse asyncResponse) {
+        workerPool.execute(() -> {
+
+            asyncResponse.resume(buildSuccessJsonResponse("It Works!"));
+
+        });
+    }
+}
+
