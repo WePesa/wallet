0536859f8528f6444f8c503c0bcf8519527c1c6b
Harsh Pokharna <harsh.81291@gmail.com>
2016-12-12 15:29:04 +0530

    Ether API added

---
+++ wepesa_backend/src/main/java/wepesa/api/EtherApi.java
@@ -0,0 +1,11 @@
+package wepesa.api;
+
+/**
+ * Created by harsh on 12/12/16.
+ */
+public interface EtherApi {
+
+    double getBalance(String address);
+
+    boolean sendTransaction(String toAddress, String fromAddress, double amount);
+}
