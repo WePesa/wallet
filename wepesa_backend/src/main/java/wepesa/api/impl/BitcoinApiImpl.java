523c19ea084e09795f0ea7ef54c96825fcf45dc7
Harsh Pokharna <harsh.81291@gmail.com>
2016-12-13 19:03:13 +0530

    Bitcoin API Impl modified

--- wepesa_backend/src/main/java/wepesa/api/impl/BitcoinApiImpl.java
+++ wepesa_backend/src/main/java/wepesa/api/impl/BitcoinApiImpl.java
@@ -16,4 +16,9 @@ public class BitcoinApiImpl implements BitcoinApi {
     public boolean sendTransaction(String toAddress, String fromAddress, double amount) {
         return false;
     }
+
+    @Override
+    public String getNewAddress() {
+        return null;
+    }
 }
