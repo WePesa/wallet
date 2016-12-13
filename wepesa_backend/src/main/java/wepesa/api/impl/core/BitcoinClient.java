ac162f7d98db6f2ee97c9cd29cf7b710ca6e55df
Harsh Pokharna <harsh.81291@gmail.com>
2016-12-13 18:57:47 +0530

    Bitcoin Client getNewAddress

--- wepesa_backend/src/main/java/wepesa/api/impl/core/BitcoinClient.java
+++ wepesa_backend/src/main/java/wepesa/api/impl/core/BitcoinClient.java
@@ -1,5 +1,11 @@
 package wepesa.api.impl.core;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 /**
  * Created by harsh on 13/12/16.
  */
@@ -8,10 +14,11 @@ public class BitcoinClient {
     public static final String API_KEY = "44b4-3005-53da-371c";
 
     private static BitcoinClient instance;
+    private static OkHttpClient okHttpClient;
 
     private BitcoinClient()
     {
-
+        okHttpClient = new OkHttpClient();
     }
 
     public static BitcoinClient getInstance()
@@ -24,4 +31,12 @@ public class BitcoinClient {
         return instance;
     }
 
+    public static Response getNewBitcoinAddress() throws IOException {
+        Request request = new Request.Builder()
+                .url("https://block.io/api/v2/get_new_address/?api_key=" + API_KEY)
+                .build();
+
+        Response response = okHttpClient.newCall(request).execute();
+        return response;
+    }
 }
